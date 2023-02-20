29710409e1319c915516aa1b7892d52606cfe0b9
Elvis Rari <46717578+rarisenpai@users.noreply.github.com>
2023-02-20 18:41:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,73 +49,66 @@ def admin():
 
 
     st.title("Pharmacy Database Dashboard")
-    menu = ["Drugs", "Orders"]
+    st.sidebar.selectbox("Drugs")
+    menu = ["Add", "View", "Update", "Delete"]
     choice = st.sidebar.selectbox("Menu", menu)
+    if choice == "Add":
+        with st.form(key='add_drug_form'):
+            st.subheader("Add Drugs")
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                drug_name = st.text_input("Enter the Drug Name")
+                drug_expiry = st.date_input("Expiry Date of Drug (YYYY-MM-DD)")
+                drug_mainuse = st.text_input("When to Use")
+            with col2:
+                drug_quantity = st.text_input("Enter the quantity")
+                drug_price = st.text_input("Enter the price")
+                drug_id = st.text_input("Enter the Drug id (example:#D1)")
+
+            if st.form_submit_button("Add Drug"):
+                drug_add_data(drug_name,drug_expiry,drug_mainuse,drug_quantity,drug_price, drug_id)
+                st.success("Successfully Added Data")
+    if choice == "View":
+        st.subheader("Drug Details")
+
+        search_term = st.text_input("Search for a drug by name")
+        search = st.checkbox("Search")
+        if search:
+            drug_result = drug_view_all_data(search_term)
+        else:
+            drug_result = drug_view_all_data()
+
+        drug_clean_df = pd.DataFrame(drug_result, columns=["Name", "Expiry Date", "Use", "Quantity","price" "ID"])
+        drug_clean_df.index+=1
+        st.table(drug_clean_df)
 
 
 
-    ## DRUGS
-    if choice == "Drugs":
+    if choice == 'Update':
+        with st.form(key='update_drug_form'):
+            st.subheader("Update Drugs")
 
-        menu = ["Add", "View", "Update", "Delete"]
-        choice = st.sidebar.selectbox("Menu", menu)
-        if choice == "Add":
-            with st.form(key='add_drug_form'):
-                st.subheader("Add Drugs")
+            col1, col2 = st.columns(2)
 
-                col1, col2 = st.columns(2)
+            with col1:
+                drug_name = st.text_input("Enter the Drug Name")
+                drug_expiry = st.date_input("Expiry Date of Drug (YYYY-MM-DD)")
+                drug_mainuse = st.text_input("When to Use")
+            with col2:
+                drug_quantity = st.text_input("Enter the quantity")
+                drug_price = st.text_input("Enter the price")
+                drug_id = st.text_input("Enter the Drug id (example:#D1)")
 
-                with col1:
-                    drug_name = st.text_input("Enter the Drug Name")
-                    drug_expiry = st.date_input("Expiry Date of Drug (YYYY-MM-DD)")
-                    drug_mainuse = st.text_input("When to Use")
-                with col2:
-                    drug_quantity = st.text_input("Enter the quantity")
-                    drug_price = st.text_input("Enter the price")
-                    drug_id = st.text_input("Enter the Drug id (example:#D1)")
-
-                if st.form_submit_button("Add Drug"):
-                    drug_add_data(drug_name,drug_expiry,drug_mainuse,drug_quantity,drug_price, drug_id)
-                    st.success("Successfully Added Data")
-        if choice == "View":
-            st.subheader("Drug Details")
-
-            search_term = st.text_input("Search for a drug by name")
-            search = st.checkbox("Search")
-            if search:
-                drug_result = drug_view_all_data(search_term)
-            else:
-                drug_result = drug_view_all_data()
-
-            drug_clean_df = pd.DataFrame(drug_result, columns=["Name", "Expiry Date", "Use", "Quantity","price" "ID"])
-            drug_clean_df.index+=1
-            st.table(drug_clean_df)
-
-
-
-        if choice == 'Update':
-            with st.form(key='update_drug_form'):
-                st.subheader("Update Drugs")
-
-                col1, col2 = st.columns(2)
-
-                with col1:
-                    drug_name = st.text_input("Enter the Drug Name")
-                    drug_expiry = st.date_input("Expiry Date of Drug (YYYY-MM-DD)")
-                    drug_mainuse = st.text_input("When to Use")
-                with col2:
-                    drug_quantity = st.text_input("Enter the quantity")
-                    drug_price = st.text_input("Enter the price")
-                    drug_id = st.text_input("Enter the Drug id (example:#D1)")
-
-                if st.form_submit_button("Update Drug"):
-                    drug_update(drug_name,drug_expiry,drug_mainuse,drug_quantity,drug_price,drug_id)
-                    st.success("Successfully Added Data")
-        if choice == 'Delete':
-            st.subheader("Delete Drugs")
-            did = st.text_input("Drug ID")
-            if st.button(label="Delete"):
-                drug_delete(did)
+            if st.form_submit_button("Update Drug"):
+                drug_update(drug_name,drug_expiry,drug_mainuse,drug_quantity,drug_price,drug_id)
+                st.success("Successfully Added Data")
+    if choice == 'Delete':
+        st.subheader("Delete Drugs")
+        did = st.text_input("Drug ID")
+        if st.button(label="Delete"):
+            drug_delete(did)
 
 if __name__ == '__main__':
     drug_create_table()
